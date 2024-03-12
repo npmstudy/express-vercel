996ec8d9fe2f7f43e06ca1d14e4a70eb6c69715a
npmstudy <npmstudy@qq.com>
2024-03-12 10:52:12 +0800

    init

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
