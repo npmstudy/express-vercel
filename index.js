7e565d11db18773f09278750d95b6e3e68a568f3
npmstudy <npmstudy@qq.com>
2024-03-12 11:01:50 +0800

    document, please ignore

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-// app.use(express.static("public"));
-// app.use("/static", express.static("public"));
-
 app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/view", (req, res) => {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
+
+// var users = [
+//   { name: "tobi", email: "tobi@learnboost.com" },
+//   { name: "loki", email: "loki@learnboost.com" },
+//   { name: "jane", email: "jane@learnboost.com" },
+// ];
+
+// app.get("/", function (req, res) {
+//   res.render("users", {
+//     users: users,
+//     title: "EJS example",
+//     header: "Some users",
+//   });
+// });
+
 
 app.get("/", (req, res) => {
   res.send("Hello World");
